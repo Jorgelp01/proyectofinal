5387bc5fc37d63c59e0f6008c4ecb6056e064b84
GarzaZazueta <garzazazueta5@gmail.com>
2015-07-06 17:23:26 -0600

    Migraciones y BD Almacen con 10 Usuarios

---
+++ database/migrations/2014_10_12_000000_create_Usuarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlumnoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Usuarios', function (Blueprint $table) {
+            $table->increments('ID');
+            $table->string('Nombre', 20);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Usuarios');
+    }
+}
